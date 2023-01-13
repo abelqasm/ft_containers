0673fe98816588e3de5edc79673d052dd1e1f645
Abderrahmane Belqasmi <abelqasm@e2r13p3.1337.ma>
2023-01-13 11:38:14 +0100

    capacity and element acces member added

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: abelqasm <abelqasm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 15:25:53 by abelqasm          #+#    #+#             */
-/*   Updated: 2023/01/11 16:11:31 by abelqasm         ###   ########.fr       */
+/*   Updated: 2023/01/12 18:08:47 by abelqasm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define VECTOR_HPP
 
 #include <memory>
+#include <mylib.hpp>
 
 namespace ft
 {
@@ -41,17 +42,18 @@ namespace ft
             typedef std::reverse_iterator<const_iterator>       const_reverse_iterator;
 
 
+        //-------------------------------------------------------------------------------------------------//
             //constructors
             explicit vector (const allocator_type& alloc = allocator_type())
             {
                 sizeElemnts = numElemnts = 0;
                 elemnts = alloc.allocate(0);
             }
-            explicit vector (size_type n, const value_type& value = value_type(), const allocator_type& alloc = allocator_type())
+            explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
             {
                 sizeElemnts = numElemnts = n;
                 elemnts = alloc.allocate(n);
-                uninitialized_fill_n(elemnts, n, value);
+                uninitialized_fill_n(elemnts, n, val);
             }
             template <class InputIterator>
             vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
@@ -66,53 +68,148 @@ namespace ft
             ~vector()
             {   
             }
-
+        
             vector& operator= (const vector& x)
             {
             }
+            vector<T,Allocator>& operator=(const vector<T,Allocator>& x);
+            template <class InputIterator>
+            void assign(InputIterator first, InputIterator last);
+            void assign(size_type n, const T& u);
+            allocator_type get_allocator() const;
             
+        //-------------------------------------------------------------------------------------------------//    
             //iterators
-            iterator               begin() noexcept;
-            const_iterator         begin()   const noexcept;
-            iterator               end() noexcept;
-            const_iterator         end()     const noexcept;
+            iterator               begin();
+            const_iterator         begin()   const;
+            iterator               end();
+            const_iterator         end()     const;
             
             //revers iterators
-            reverse_iterator       rbegin() noexcept;
-            const_reverse_iterator rbegin()  const noexcept;
-            reverse_iterator       rend() noexcept;
-            const_reverse_iterator rend()    const noexcept;
-            
+            reverse_iterator       rbegin();
+            const_reverse_iterator rbegin()  const;
+            reverse_iterator       rend();
+            const_reverse_iterator rend()    const;
 
+        //-------------------------------------------------------------------------------------------------//
+            // Element access
+            reference       operator[](size_type n)
+            {
+                return  elemnts[n];
+            }
+            const_reference operator[](size_type n) const
+            {
+                return  elemnts[n];
+            }
+            reference       at(size_type n)
+            {
+                if (n >= numElemnts)
+                    throw std::out_of_range("out of range");
+                return elemnts[n];  
+            }
+            const_reference at(size_type n) const
+            {
+                if (n >= numElemnts)
+                    throw std::out_of_range("out of range");
+                return elemnts[n];
+            }
 
+            reference       front()
+            {
+                return elemnts[0];
+            }
+            const_reference front() const
+            {
+                return elemnts[0];
+            }
+            reference       back()
+            {
+                if (numElemnts == 0)
+                    return elemnts[0];
+                return elemnts[numElemnts - 1];
+            }
+            const_reference back() const
+            {
+                if (numElemnts == 0)
+                    return elemnts[0];
+                return elemnts[numElemnts - 1];
+            }
+
+            value_type*       data()
+            {
+                return elemnts;
+            }
+            const value_type* data() const
+            {
+                return elemnts;
+            }
+
+        //-------------------------------------------------------------------------------------------------//
             // Capacity member functions
-            size_type size() const noexcept
+            size_type size() const
             {
                 return numElemnts;
             }
-            size_type max_size() const noexcept
+            size_type max_size() const
             {
+                return alloc.max_size();
             }
-            size_type capacity() const noexcept
+            size_type capacity() const
             {
                 return sizeElemnts;
             }
-            bool empty() const noexcept
+            bool empty() const
             {
-                return (numElemnts == 0);
+                return numElemnts == 0;
             }
             void reserve(size_type n)
             {
                 if (n <= sizeElemnts)
                     return;
                 T* tempMem = alloc.allocate(n);
-                uninitialized_copy(elemnts, elemnts + numElemnts, tempMem);
+                my_uninitialized_copy(elemnts, elemnts + numElemnts, tempMem);
                 for (size_type i = 0; i < numElemnts; i++)
                     alloc.destroy(&elemnts[i]);
                 alloc.deallocate(elemnts, sizeElemnts);
                 elemnts = tempMem;
                 sizeElemnts = n;
             }
+            void resize (size_type n, value_type val = value_type())
+            {
+                if (n <= numElemnts)
+                {
+                    for (size_type i = n; i < sizeElemnts; i++)
+                        alloc.destroy(&elemnts[i]);
+                    numElemnts = n;
+                    return;
+                }
+                else
+                    reserve(n);
+                my_uninitialized_fill_n(elemnts + numElemnts, n - numElemnts, val);
+                numElemnts = n;
+            }
+
+        //-------------------------------------------------------------------------------------------------//
+            //Modifiers
+            iterator insert (iterator position, const value_type& val)
+            {
+                if (numElemnts + 1 > sizeElemnts)
+                    reserve(sizeElemnts * 2);
+                my_uninitialized_copy(elemnts + (position - elemnts), elemnts + numElemnts, elemnts + (position - elemnts) + 1);
+                alloc.construct(elemnts + (position - elemnts), val);
+                numElemnts++;
+                return position;
+            }
+            void insert (iterator position, size_type n, const value_type& val)
+            {
+                if (numElemnts + n > sizeElemnts)
+                    reserve(sizeElemnts * 2);
+                my_uninitialized_copy(elemnts + (position - elemnts), elemnts + numElemnts, elemnts + (position - elemnts) + n);
+                my_uninitialized_fill_n(elemnts + (position - elemnts), n, val);
+                numElemnts += n;
+            }
+            template <class InputIterator>
+            void insert (iterator position, InputIterator first, InputIterator last);
     };
 }
 
