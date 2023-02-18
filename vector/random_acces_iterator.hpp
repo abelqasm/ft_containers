fce28d24912ae3f69fdbeabce8c420bc7eca7106
Abderrahmane Belqasmi <abelqasm@e2r12p1.1337.ma>
2023-02-18 21:09:30 +0100

    const iterators for the map fixed

--- vector/random_acces_iterator.hpp
+++ vector/random_acces_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: abelqasm <abelqasm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 17:11:26 by abelqasm          #+#    #+#             */
-/*   Updated: 2023/02/16 15:45:40 by abelqasm         ###   ########.fr       */
+/*   Updated: 2023/02/18 21:09:19 by abelqasm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ namespace ft
     class random_acces_iterator
     {
     public:
-            typedef typename iterator_traits<T>::difference_type         difference_type;
-            typedef typename iterator_traits<T>::value_type              value_type;
-            typedef typename iterator_traits<T>::pointer                 pointer;
-            typedef typename iterator_traits<T>::reference               reference;
-            typedef typename std::random_access_iterator_tag             iterator_category;
+            typedef typename ft::iterator_traits<T>::difference_type         difference_type;
+            typedef typename ft::iterator_traits<T>::value_type              value_type;
+            typedef typename ft::iterator_traits<T>::pointer                 pointer;
+            typedef typename ft::iterator_traits<T>::reference               reference;
+            typedef typename std::random_access_iterator_tag                 iterator_category;
     private:
             pointer _ptr;
     public:
